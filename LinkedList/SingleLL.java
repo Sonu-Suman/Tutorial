a35c3d5dd1bc27f379218acf8e3e1ccffddffbf6
Aparajita suman <sumankumar86028@gmail.com>
2022-04-06 18:32:33 +0530

    This is for Linked List folder

--- LinkedList/SingleLL.java
+++ LinkedList/SingleLL.java
@@ -1,7 +1,38 @@
 package LinkedList;
 
 public class SingleLL {
-    public static void main(String[] args) {
-        System.out.println("Hello suman");
+    private Node head;
+
+    public void insertHead(int data){
+        if (head==null){
+            head = new Node(data);
+        } else {
+            Node temp = new Node(data);
+
+            temp.next = head;
+            head = temp;
+        }
+    }
+
+    public void print() {
+        Node temp = head;
+
+        while(temp!=null){
+            if(temp.next==null){
+                System.out.print(temp.data);
+                break;
+            }
+            System.out.print(temp.data+" --> ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args){
+        SingleLL ll = new SingleLL();
+        ll.insertHead(5);
+        ll.insertHead(9);
+        ll.insertHead(7);
+
+        ll.print();
     }
 }
